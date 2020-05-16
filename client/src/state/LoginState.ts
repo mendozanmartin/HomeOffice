9b9e37f1c133a22f7f5c56d5b32e06d0313cb5d7
Martin Mendoza <mendozanmartin@gmail.com>
2020-05-16 12:54:48 -0400

    UI changes

--- client/src/state/LoginState.ts
+++ client/src/state/LoginState.ts
@@ -4,26 +4,25 @@ import StateManager from "./StateManager";
 import GameState from "./GameState";
 
 export default class LoginState extends AState {
+  private playBtn: HTMLButtonElement;
 
-    private playBtn: HTMLButtonElement;
+  constructor() {
+    super("login");
+    this.playBtn = GetElementById("login-play") as HTMLButtonElement;
+    this.playBtn.addEventListener("click", this.playGame);
+  }
 
-    constructor() {
-        super("login");
-        this.playBtn = GetElementById('login-play') as HTMLButtonElement;
-        this.playBtn.addEventListener('click', this.playGame);
-    }
+  /**
+   * life cycle method
+   *
+   * called when the screen will become no longer visible
+   */
+  public destroy() {
+    console.log("destroying login");
+    this.playBtn.removeEventListener("click", this.playGame);
+  }
 
-    /**
-     * life cycle method
-     * 
-     * called when the screen will become no longer visible
-     */
-    public destroy() {
-        console.log('destroying login')
-        this.playBtn.removeEventListener("click", this.playGame);
-    }
-
-    playGame = async () => {
-        StateManager.GetInstance().Push(await GameState.Create());
-    }
+  playGame = async () => {
+    StateManager.GetInstance().Push(await GameState.Create());
+  };
 }
