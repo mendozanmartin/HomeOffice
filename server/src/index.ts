0ee447191d36d772b37b180d73fd90e5a59d02ee
Alec Di Vito <divito.alec@gmail.com>
2020-05-17 02:02:08 -0400

    updated server

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,5 +5,5 @@ const port = parseInt(process.env.PORT ?? "5000");
 const server = new Server(port);
 
 server.listen(port => {
-    console.log(`Server is listening on http://localhost:${port}`);
+    console.log(`Server is listening on https://localhost:${port}`);
 });
